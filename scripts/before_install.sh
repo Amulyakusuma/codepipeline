7811fef0be29c7ac33b7b42986e739ff171022e3
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-03 12:00:46 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ubuntu/server
-
-#_Update_&_Set_Node_Version
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-
-#_Download_Node_&NPM
-sudo apt-get install -y nodejs
-
-#_Download_PM2
-sudo npm install pm2@latest -g
+APP_DIR=/var/www/html/application
