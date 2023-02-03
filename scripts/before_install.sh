db224084ec3922db9cc310069bc06e7905d2339a
Amulyakusuma <112145354+Amulyakusuma@users.noreply.github.com>
2023-02-03 11:48:30 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@
 cd /home/ubuntu/server
 
 #_Update_&_Set_Node_Version
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
 #_Download_Node_&NPM
 sudo apt-get install -y nodejs
